77cea055ae8c406db31a30586f84f084ef7e5b50
Ana-Gabriel <agp@odoo.com>
2020-03-20 13:35:50 -0700

    [ADD] auto_lot_info: add logging

--- auto_lot_info/models/manufacturing_order.py
+++ auto_lot_info/models/manufacturing_order.py
@@ -1,16 +1,22 @@
 # -*- coding: utf-8 -*-
+import logging
+
 from odoo import api, fields, models
+
+_logger = logging.getLogger(__name__)
 
 class MrpProductProduce(models.TransientModel):
     _inherit = 'mrp.product.produce'
 
     def do_produce(self):
         for s in self:
-            lot1 = s.finished_lot_id
-            for m in s.raw_workorder_line_ids:
+            lot1 = s['finished_lot_id']
+            for m in s['raw_workorder_line_ids']:
                 if lot1: 
-                    lot2 = m.lot_id
+                    _logger.info("lot1: {}".format(lot1['name']))
+                    lot2 = m['lot_id']
                     if lot2:
-                        lot1[0].produced_from = lot2[0]
+                        _logger.info("lot2: {}".format(lot2['name']))
+                        lot1[0]['produced_from'] = lot2[0]
         
         return super(MrpProductProduce, self).do_produce()

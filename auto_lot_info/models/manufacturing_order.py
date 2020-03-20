fa872e94f68da5f7adc66ba570d719d17774085c
Ana-Gabriel <agp@odoo.com>
2020-03-20 13:25:06 -0700

    [FIX] auto_lot_info: set produced_from before calling super

--- auto_lot_info/models/manufacturing_order.py
+++ auto_lot_info/models/manufacturing_order.py
@@ -5,17 +5,12 @@ class MrpProductProduce(models.TransientModel):
     _inherit = 'mrp.product.produce'
 
     def do_produce(self):
-        result = super(MrpProductProduce, self).do_produce()
-
-        print()
         for s in self:
             lot1 = s.finished_lot_id
-            print(lot1)
             for m in s.raw_workorder_line_ids:
                 if lot1: 
                     lot2 = m.lot_id
-                    print(lot2)
                     if lot2:
                         lot1[0].produced_from = lot2[0]
-
-        return result
+        
+        return super(MrpProductProduce, self).do_produce()

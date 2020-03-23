de8ae819c9429a343490561a22eafdf80727ac10
Ana-Gabriel <agp@odoo.com>
2020-03-23 15:42:56 -0700

    [FIX] oldpalllc_mrp: name was wrong

--- oldpalllc_mrp/models/manufacturing_order.py
+++ oldpalllc_mrp/models/manufacturing_order.py
@@ -8,7 +8,7 @@ class MrpProductProduce(models.TransientModel):
     def do_produce(self):
         for record in self.filtered(lambda rec: rec.lot_id):
             for raw_worker_order in record.raw_workorder_line_ids.filtred(lambda wl: wl.lot_id): 
-                record.lot_id.produced_from = raw_worker_order.lot_id
+                record.lot_id.produced_from_id = raw_worker_order.lot_id
                 record.lot_id._change_produced_from()
         
         return super(MrpProductProduce, self).do_produce()

d11ebe5d0b8047f5fb677f80c4ac5f888a4e7769
Ana-Gabriel <agp@odoo.com>
2020-03-23 15:54:19 -0700

    [FIX] oldpalllc_mrp: wrong filtering field on MO record

--- oldpalllc_mrp/models/manufacturing_order.py
+++ oldpalllc_mrp/models/manufacturing_order.py
@@ -6,9 +6,9 @@ class MrpProductProduce(models.TransientModel):
     _inherit = 'mrp.product.produce'
 
     def do_produce(self):
-        for record in self.filtered(lambda rec: rec.lot_id):
+        for record in self.filtered(lambda rec: rec.finished_lot_id):
             for raw_worker_order in record.raw_workorder_line_ids.filtred(lambda wl: wl.lot_id): 
-                record.lot_id.produced_from_id = raw_worker_order.lot_id
-                record.lot_id._change_produced_from()
+                record.finished_lot_id.produced_from_id = raw_worker_order.lot_id
+                record.finished_lot_id._change_produced_from()
         
         return super(MrpProductProduce, self).do_produce()

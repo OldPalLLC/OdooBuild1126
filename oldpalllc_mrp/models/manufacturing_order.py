ca3e711dc3c7b9d0681ed48471b39a3ba8664a79
Ana-Gabriel <agp@odoo.com>
2020-04-01 13:27:30 -0700

    [ADD] oldpalllc_mrp: mo_category to mrp_production

--- oldpalllc_mrp/models/manufacturing_order.py
+++ oldpalllc_mrp/models/manufacturing_order.py
@@ -7,8 +7,16 @@ class MrpProductProduce(models.TransientModel):
 
     def do_produce(self):
         for record in self.filtered(lambda rec: rec.finished_lot_id):
+            mrp = record.production_id
             for raw_worker_order in record.raw_workorder_line_ids.filtered(lambda wl: wl.lot_id): 
                 record.finished_lot_id.produced_from_id = raw_worker_order.lot_id
-                record.finished_lot_id._change_produced_from()
+                record.finished_lot_id.production_id = mrp
+                if mrp.mo_category != 'testing': 
+                    record.finished_lot_id._change_produced_from()
         
         return super(MrpProductProduce, self).do_produce()
+
+class MrpProduction(models.Model):
+    _inherit = 'mrp.production'
+
+    mo_category = fields.Selection(string='MO Category', selection=[('testing', 'Testing'), ('finished', 'Finished Goods')], required=True)
